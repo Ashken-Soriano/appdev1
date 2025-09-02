d0e31541adb36a2440777c112ec4a5a5b2065483
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-09-02 23:07:30 +0800

    Activity 10: Add React About Me project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>About Me</h1>
+
+        <p><strong>Name:</strong> Soriano, Rain Louie Ashken G.</p>
+        <p><strong>Course/Year:</strong> BS Information Technology 3rd Year</p>
+        <p><strong>Fun Fact:</strong> I can make a game using ChatGPT</p>
+
+        <hr />
+
+        <h2>Why I Want to Learn Git & GitHub</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This tool is really useful for me, because it helps me track my code. 
+          It can also work with my classmates and other people on projects. 
+          It can help me create my own portfolio to show my skills. 
+          Git & GitHub are common tools used by developers, and learning them 
+          prepares me for real projects and future jobs.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
