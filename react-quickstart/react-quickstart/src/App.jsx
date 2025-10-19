aad8a81d749b97b0e98e574d1a5ba24654138bf2
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-10-19 15:55:16 +0800

    Done displaying data

--- react-quickstart/react-quickstart/src/App.jsx
+++ react-quickstart/react-quickstart/src/App.jsx
@@ -7,14 +7,29 @@ import './App.css'
 
 
 function App() {
+  const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
   // const [count, setCount] = useState(0)
   return (
-    <> 
-      <img className="avatar" src='https://media.licdn.com/dms/image/C5612AQHUdkN8xk0qOA/article-cover_image-shrink_720_1280/0/1520083596181?e=2147483647&v=beta&t=tDJ--14IMk99iejwcrmgxkSWhkq7s-tiffoYGDVMZSA' />
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
+  <>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
-  )
+  );
 }
+
+
 
 export default App
