5280911d0b2689c49bea86be83cf8e898ef7ffb6
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-10-19 15:51:19 +0800

    Done adding styles

--- react-quickstart/react-quickstart/src/App.jsx
+++ react-quickstart/react-quickstart/src/App.jsx
@@ -8,9 +8,9 @@ import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
-
   return (
-    <>
+    <> 
+      <img className="avatar" src='https://media.licdn.com/dms/image/C5612AQHUdkN8xk0qOA/article-cover_image-shrink_720_1280/0/1520083596181?e=2147483647&v=beta&t=tDJ--14IMk99iejwcrmgxkSWhkq7s-tiffoYGDVMZSA' />
       <h1>About</h1>
       <p>Hello there.<br />How do you do?</p>
     </>
