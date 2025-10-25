0b30312feca30690d90bbdf5edc20c18e1799084
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-10-25 09:09:35 +0800

    Done with how to render multiple components at a time

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,9 +1,18 @@
+function Friend({ name }) {
+  return <li>{name}</li>;
+}
+
 export default function App() {
-  const isLoggedIn = false;
+  const friends = ["Liam", "Noah", "Emma", "Olivia"];
 
   return (
     <div>
-      {isLoggedIn ? <h1>Welcome back!</h1> : <h1>Please log in.</h1>}
+      <h2>Friends List</h2>
+      <ul>
+        {friends.map((friend) => (
+          <Friend key={friend} name={friend} />
+        ))}
+      </ul>
     </div>
   );
 }
