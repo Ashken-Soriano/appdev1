7832288de4a3a763e6b1d8ebde8b76fbebf75ecc
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-12-11 18:15:59 +0800

    Done with more fun with JavaScript objects and curly braces

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 // src/App.jsx
+const person = {
+  name: 'Taylor',
+  imageId: '7vQD0fP',
+  imageType: 'jpg',
+  theme: {
+    backgroundColor: 'black',
+    color: 'pink'
+  }
+};
+
 export default function TodoList() {
   return (
-    <div style={{ backgroundColor: 'black', color: 'pink' }}>
-      <h1>Hedy Lamarr's To Do List</h1>
+    <div style={person.theme}>
+      <h1>{person.name}'s To Do List</h1>
       <img
         className="avatar"
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
+        src={"https://i.imgur.com/" + person.imageId + "." + person.imageType}
+        alt={person.name}
       />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fueled engine</li>
+      </ul>
     </div>
   );
 }
