779631d385f2aadb18284972673a03dd5d3e0c08
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-12-11 18:03:34 +0800

    Done with using curly braces: A window into the JavaScript world

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+// src/App.jsx
+const user = {
+  name: 'Gregorio Y. Zara',
+  imageUrl: 'https://i.imgur.com/QIrZWGIs.jpg',
+};
+
 export default function Profile() {
   return (
-    <img
-      src="https://i.imgur.com/QIrZWGIs.jpg"
-      alt="Gregorio Y. Zara"
-    />
+    <div>
+      {/* curly braces for dynamic text */}
+      <h1>{user.name}'s Profile</h1>
+
+      {/* curly braces for dynamic attribute values */}
+      <img
+        src={user.imageUrl}
+        alt={user.name}
+      />
+
+    </div>
   );
 }
