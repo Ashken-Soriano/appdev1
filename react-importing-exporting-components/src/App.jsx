ae4aa3db8dd744ac66d414fa76329747a7b75860
Zabbyyy <rainlouieashkensoriano@student.laverdad.edu.ph>
2025-12-11 15:43:00 +0800

    Done with exporting and importing multiple components from the same file

--- react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/src/App.jsx
@@ -1,10 +1,13 @@
 import Greeting from './Greeting';
+import { WelcomeMessage, GoodbyeMessage } from './Messages';
 
 function App() {
   return (
     <div>
       <h1>Root Component File</h1>
       <Greeting />
+      <WelcomeMessage />
+      <GoodbyeMessage />
     </div>
   );
 }
